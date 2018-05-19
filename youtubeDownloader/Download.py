01ab2e936ba003d53309fa61087e0d6834d31899
Alexander Mueller <alzandermuller@gmail.com>
2018-05-19 12:07:41 -0400

    Add Progress Bar, Error Handling.

--- youtubeDownloader/Download.py
+++ youtubeDownloader/Download.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+# -*- coding: utf-8 -*-
+
 import os
 import sys
 from os import makedirs
@@ -34,16 +36,64 @@ def main(argc, argv):
     filepath = '%s/%s' % (location, name) if len(urls) > 0 and name != '' else location 
     
     if not isdir(filepath):
-            makedirs(filepath)
+        makedirs(filepath)
     
     for video_url in urls:
-        video = YouTube(video_url) 
+        video = YouTube(video_url, on_progress_callback=on_progress) 
 
-        print 'Downloading: ' + video.title
-
-        video.streams.filter(progressive = True, file_extension = 'mp4').order_by('resolution').desc().first().download(filepath)
+        try:
+            print 'Downloading: ' + video.title
+            video.streams.filter(progressive = True, file_extension = 'mp4').order_by('resolution').desc().first().download(filepath)
+        except KeyboardInterrupt:
+            sys.exit()
 
     print 'Finished!'
+
+def on_progress(stream, chunk, file_handle, bytes_remaining):
+    """On download progress callback function.
+    :param object stream:
+        An instance of :class:`Stream <Stream>` being downloaded.
+    :param file_handle:
+        The file handle where the media is being written to.
+    :type file_handle:
+        :py:class:`io.BufferedWriter`
+    :param int bytes_remaining:
+        How many bytes have been downloaded.
+    """
+    filesize       = stream.filesize
+    bytes_received = filesize - bytes_remaining
+    display_progress_bar(bytes_received, filesize)
+
+def display_progress_bar(bytes_received, filesize, ch='=', scale=0.55):
+    """Display a simple, pretty progress bar.
+    Example:
+    ~~~~~~~~
+    PSY - GANGNAM STYLE() MV.mp4
+    |----------------| 100.0%
+    :param int bytes_received:
+        The delta between the total file size (bytes) and bytes already
+        written to disk.
+    :param int filesize:
+        File size of the media stream in bytes.
+    :param ch str:
+        Character to use for presenting progress segment.
+    :param float scale:
+        Scale multipler to reduce progress bar size.
+    """
+    _, columns = get_terminal_size()
+    max_width  = int(columns * scale)
+    filled     = int(round(max_width * bytes_received / float(filesize)))
+    remaining  = max_width - filled
+    bar        = ch * filled + ' ' * remaining
+    percent    = round(100.0 * bytes_received / float(filesize), 1)
+    text       = '-->  [{bar}] {percent}%\r'.format(bar=bar, percent=percent)
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
+def get_terminal_size():
+    """Return the terminal size in rows and columns."""
+    rows, columns = os.popen('stty size', 'r').read().split()
+    return int(rows), int(columns)
 
 if __name__ == '__main__':
    main(len(sys.argv) - 1, sys.argv[1:])
